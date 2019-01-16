54dd234039026b4f95d47b80e8f65f9e16dd1731
Ubuntu <ubuntu@ip-172-31-18-211.ap-northeast-1.compute.internal>
2019-01-16 07:07:22 +0000

    Definded router and completed the part of getting data by id

--- app/config/router.php
+++ app/config/router.php
@@ -4,4 +4,7 @@ $router = $di->getRouter();
 
 // Define your routes here
 
+$router->add('/items/{id:[0-9]+}', 'Items::index');
+
+
 $router->handle();
--- app/controllers/ItemsController.php
+++ app/controllers/ItemsController.php
@@ -1,41 +1,64 @@
 <?php
 
-use Phalcon\Mvc\Model\Criteria;
-use Phalcon\Paginator\Adapter\Model as Paginator;
 use Phalcon\Http\Request;
-
+use Phalcon\Http\Response;
 
 class ItemsController extends ControllerBase
 {
 
-	public function indexAction()
-	{
-		//
-	}
-
-	public function getAllAction()
+	public function indexAction($id)
 	{
 		$this->view->disable();
-		$request = new Request();		
+		$request = new Request();				
+		$response = new Response();
+		
+	
+		// Get data by id
+		if (!empty($id) && $request->isGet()){
+			# echo $id;
+			$item = Items::findFirst($id);	
+			
+			if ($item == true){
+				$data[] = [
+					'id' => $item->id,
+					'title' => $item->title,
+				];
+			
+				$response->setJsonContent(
+					[
+						'status' => 'FOUND',
+						'data' => $data,
+					], JSON_UNESCAPED_UNICODE
+				);
+			}
+			else {
+				$response->setJsonContent(['status' => 'Not Found']);
+			}
+			return $response;
+		}
+		
 
-		if ($request->isGet())
-		{
+		// Get all the data from db
+		if (empty($id) && $request->isGet())
+		{	
 			$items = Items::find();
 			
 			foreach ($items as $item){
 				$data[] =[
 					'id' => $item->id,
 					'title' => $item->title,
-					'price' => $item->price,
-					'description' => $item->description,
 				];
 			}
-			echo json_encode($data, JSON_UNESCAPED_UNICODE);
-		
-		}
-		else
-		{
-			echo 'Please use GET method!';
+			
+			$response->setStatusCode(200, 'OK');
+			$response->setJsonContent(
+				[
+					'status' => 'OK',
+					'data' => $data,
+				], JSON_UNESCAPED_UNICODE
+			);
+			
+			return $response;
 		}
 		
 	}
